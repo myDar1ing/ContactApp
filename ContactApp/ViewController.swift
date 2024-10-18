fb2664d830c8f5f7db4a4484156855484d7a344a
Adilet Kenesbekov <bbddbbd2003@gmail.com>
2024-10-18 20:02:42 +0500

    Initial Commit

---
+++ ContactApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactApp
+//
+//  Created by Adilet Kenesbekov on 18.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
